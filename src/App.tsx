127c76d0d1b181d1273485fb3fdd27e016bedf83
Ruslan Khasanshin <r.m.khasanshin@gmail.com>
2021-01-08 23:27:19 +0600

    Implemented working alpha version

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import './App.css';
-import React, { useCallback, useState } from "react";
+import { useCallback, useState } from "react";
 
 function App() {
-    const [file, setFile] = useState<Blob>(new Blob());
-    const [response, setResponse] = useState<Blob>(new Blob());
-    const handleChange = useCallback((event) => {
-        setFile(event.target.files[0]);
-    }, [setFile]);
-    const handleSubmit = useCallback(() => {
+  const [file, setFile] = useState<Blob | undefined>(undefined);
+  const [result, setResult] = useState<Blob | undefined>(undefined);
+  const handleChange = useCallback((event) => {
+    setFile(event.target.files[0]);
+  }, [setFile]);
+  const handleSubmit = useCallback(
+    (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      if (file !== undefined) {
         const formData = new FormData();
         formData.set("file", file);
-        fetch('http://localhost:8080/', {
-            method: 'POST',
-            body: formData
+        fetch('http://localhost:8080', {
+          method: 'POST',
+          body: formData
         })
-            .then((response) => response.blob())
-            .then((blob) => {
-                alert(blob);
-                setResponse(blob);
-            })
-            .catch((e) => alert(JSON.stringify(e)));
-    }, [file, setResponse]);
-    return (
-        <div className="App">
-          <form onSubmit={handleSubmit}>
-            <input type="file" onChange={handleChange} />
-            <input type="submit" />
-          </form>
-        </div>
-    );
+          .then((response) => response.blob())
+          .then((blob) => setResult(blob))
+          .catch((e) => alert(e));
+      }
+    },
+    [file, setResult]
+  );
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input type="file" onChange={handleChange} />
+        <input type="submit" disabled={file === undefined} />
+      </form>
+      {result && (
+        <img src={URL.createObjectURL(result)} alt="result" />
+      )}
+    </div>
+  );
 }
 
 export default App;
